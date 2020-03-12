dbf83014c5b56cd7f13dde51c091603ac9e438d0
aland1013 <adam.land@gmail.com>
2020-03-11 20:34:04 -0600

    attempted stretch project

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,37 @@
+updateTimer = () => {
+  msTens.textContent++;
+  if (msTens.textContent === '10') {
+    msTens.textContent = '0';
+    msHundreds.textContent++;
+    if(msHundreds.textContent == '10') {
+      msHundreds.textContent = '0';
+      secondOnes.textContent++;
+      if (secondOnes.textContent === '10') {
+        secondOnes.textContent = '0'
+        secondTens.textContent = '1'
+      }
+      
+    }
+  }
+}
+
+const secondTens = document.querySelector('#secondTens');
+secondTens.textContent = '0';
+
+const secondOnes = document.querySelector('#secondOnes');
+secondOnes.textContent = '0';
+
+const msHundreds = document.querySelector('#msHundreds');
+msHundreds.textContent = '0';
+
+const msTens = document.querySelector('#msTens');
+msTens.textContent = '0';
+
+
+let timer = window.setInterval(updateTimer, 10);
+
+setTimeout(() => { 
+  clearInterval(timer); 
+  document.querySelectorAll('.digit').forEach(digit => digit.style.color = 'red');
+}, 10000);
+
